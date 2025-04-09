e04ad2fdcdb48ad3a7353de90127a5a129f1ae03
Gabriel Cortes <gabriel.a.rocha23@icloud.com>
2025-04-08 23:35:27 -0300

    Entrega de Desafio

---
+++ src/main/java/dio/padroesProjeto/gof/service/ClienteService.java
@@ -0,0 +1,17 @@
+package dio.padroesProjeto.gof.service;
+
+import dio.padroesProjeto.gof.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
